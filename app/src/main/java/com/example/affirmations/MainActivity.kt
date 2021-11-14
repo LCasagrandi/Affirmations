172f89146d17148bef70bb70de504cfc34e00c2d
LCasagrandi <leonardocsgr@gmail.com>
2021-11-14 10:35:19 -0300

    Adicao da Recycler View

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,16 +2,15 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
-import com.example.affirmations.model.Affirmation
+import com.example.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView: TextView = findViewById<TextView>(R.id.textView)
+
 
     }
 }
